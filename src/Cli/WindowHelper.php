84be20f775596474b4414fa03c44e2168dd41d18
Daniel Corn <d@cundd.net>
2016-05-08 17:16:55 +0200

    Added CLI window helper to determine the width and height of the terminal window

---
+++ src/Cli/WindowHelper.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 08/05/16
+ * Time: 16:10
+ */
+
+namespace Cundd\TestFlight\Cli;
+
+/**
+ * CLI window helper
+ */
+class WindowHelper
+{
+    /**
+     * @var int
+     */
+    private $width;
+
+    /**
+     * @var int
+     */
+    private $height;
+
+    /**
+     * Returns the window's width
+     *
+     * @example
+     *  assert(is_int((new \Cundd\TestFlight\Cli\WindowHelper)->getWidth()));
+     *  assert(0 < (new \Cundd\TestFlight\Cli\WindowHelper)->getWidth());
+     * @return int
+     */
+    public function getWidth()
+    {
+        if ($this->width === null) {
+            $this->width = intval($this->systemCall('tput cols'));
+        }
+
+        return $this->width;
+    }
+
+    /**
+     * Returns the window's height
+     *
+     * @example
+     *  assert(is_int((new \Cundd\TestFlight\Cli\WindowHelper)->getHeight()));
+     *  assert(0 < (new \Cundd\TestFlight\Cli\WindowHelper)->getHeight());
+     * @return int
+     */
+    public function getHeight()
+    {
+        if ($this->height === null) {
+            $this->height = intval($this->systemCall('tput lines'));
+        }
+
+        return $this->height;
+    }
+
+    /**
+     * @param string $command
+     * @return string
+     */
+    private function systemCall(string $command)
+    {
+        if (is_callable('exec')) {
+            return exec($command);
+        }
+
+        return '';
+    }
+}
