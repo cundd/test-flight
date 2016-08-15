f2c1d0b0a5a39588d713ee6c97e99e283d40e0fa
Daniel Corn <d@cundd.net>
2016-08-15 20:04:44 +0200

    Fixed a bug in the autoload finder

--- src/Autoload/Finder.php
+++ src/Autoload/Finder.php
@@ -46,7 +46,8 @@ class Finder
             if ($autoloaderPath !== '') {
                 return $autoloaderPath;
             }
-        } while ($basePath = dirname($basePath));
+            $basePath = dirname($basePath);
+        } while ($basePath && $basePath !== '/');
 
         return '';
     }
