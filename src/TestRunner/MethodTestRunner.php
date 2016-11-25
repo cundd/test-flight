33348e009d780466a1bd4584a4806c27d9f1c56a
Daniel Corn <daniel@iresults.li>
2016-11-25 15:35:37 +0100

    Added support to setUp() on test instances

--- src/TestRunner/MethodTestRunner.php
+++ src/TestRunner/MethodTestRunner.php
@@ -26,6 +26,7 @@ class MethodTestRunner extends AbstractTestRunner
     protected function performTest(DefinitionInterface $definition, ContextInterface $context)
     {
         $instance = $this->objectManager->create($definition->getClassName());
+        $this->invokeSetup($instance);
         $methodName = $definition->getMethodName();
 
         if ($definition->getMethodIsPublic()) {
@@ -34,6 +35,24 @@ class MethodTestRunner extends AbstractTestRunner
             $method = $definition->getReflectionMethod();
             $method->setAccessible(true);
             $method->invoke($instance, $context);
+        }
+    }
+
+    /**
+     * @param $instance
+     */
+    protected function invokeSetup($instance)
+    {
+        if (!method_exists($instance, 'setUp')) {
+            return;
+        }
+
+        if (is_callable([$instance, 'setUp'])) {
+            $instance->setUp();
+        } else {
+            $setUpMethodReflection = new \ReflectionMethod(get_class($instance), 'setUp');
+            $setUpMethodReflection->setAccessible(true);
+            $setUpMethodReflection->invoke($instance);
         }
     }
 }
